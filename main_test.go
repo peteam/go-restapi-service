813965615126a3aeb1b0af19436427fd77eed0ea
srikanthc <csrikanth2016@gmail.com>
2019-07-17 15:35:39 +0530

    test

--- main_test.go
+++ main_test.go
@@ -11,19 +11,19 @@ import (
 func TestMain(t *Testing.T) {
 	result := hello(Srikanth)
 	
-	if result != "Hello Srikanth" {
+	if (result != "Hello Srikanth") {
 		t.Errorf("hello failed, expected %v, got  %v", "Hello Srikanth!", result)
 	} else {
-		t.Logf(("hello success, expected %v, got %v", "Hello Srikanth!", result)
+		t.Logf("hello success, expected %v, got %v", "Hello Srikanth!", result)
 	}
 	
 	
 	result1 := hello2(Srikanth)
 	
-	if result1 != "Hello Srikanth" {
+	if (result1 != "Hello Srikanth") {
 		t.Errorf("hello2 failed, expected %v, got %v", "Hello Srikanth", result1)
 	} else {
-		t.Logf(("hello2 success, expected %v, got %v", "Hello Srikanth", result)
+		t.Logf("hello2 success, expected %v, got %v", "Hello Srikanth", result1)
 	}
 	
 	
